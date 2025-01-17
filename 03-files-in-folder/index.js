8fedc5997ab92d935865de8a9f268dedc6443964
Matvey Lebedev <matvey.swansii@gmail.com>
2025-01-17 17:02:53 +0300

    feat: completed task-3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathFolder, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.log('Ошибка!');
+  } else {
+    files.forEach((item) => {
+      if (item.isFile()) {
+        const pathFile = path.join(pathFolder, item.name);
+
+        fs.stat(pathFile, (err, stat) => {
+          if (err) {
+            console.log('Ошибка!');
+          } else {
+            const name = path.basename(pathFile, path.extname(pathFile));
+            const extension = path.extname(pathFile);
+            console.log(
+              `${name} - ${extension.slice(1)} - ${stat.size / 1000} kB`,
+            );
+          }
+        });
+      }
+    });
+  }
+});
