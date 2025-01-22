0ab072f251f8999ee031baad17c09f2e077d4c2a
Matvey Lebedev <147207105+swansii1@users.noreply.github.com>
2025-01-23 02:02:28 +0300

    feat: when the contents of the main folder are changed, the copied folder is updated.

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,21 +5,24 @@ const copyFolder = path.join(__dirname, 'copy-files');
 const pathFolderCopy = path.join(__dirname, 'files');
 
 function copyDir(src, dest) {
-  fs.rm(dest, { recursive: true }, (err) => {
+  fs.rm(dest, { recursive: true, force: true }, (err) => {
     if (err) {
       console.log('Ошибка при обновлении/удалении папки:', err);
       return;
     }
+
     fs.mkdir(dest, (err) => {
       if (err) {
         console.log('Ошибка при создании папки:', err);
         return;
       }
+
       fs.readdir(src, (err, files) => {
         if (err) {
           console.log('Ошибка при чтении папки:', err);
           return;
         }
+
         files.forEach((file) => {
           const srcFile = path.join(src, file);
           const destFile = path.join(dest, file);
@@ -27,6 +30,8 @@ function copyDir(src, dest) {
           fs.copyFile(srcFile, destFile, (err) => {
             if (err) {
               console.log(`Ошибка при копировании файла ${file}:`, err);
+            } else {
+              console.log(`Скопирован файл: ${srcFile} в ${destFile}`);
             }
           });
         });
@@ -34,4 +39,5 @@ function copyDir(src, dest) {
     });
   });
 }
+
 copyDir(pathFolderCopy, copyFolder);
